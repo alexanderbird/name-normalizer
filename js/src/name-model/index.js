7a46ca5cdcc497032925cf2d799dbb1f4a8bf8b8
Alexander Bird <alex@alexanderbird.software>
2019-06-30 09:19:24 -0600

    tcr commit

--- js/src/name-model/index.js
+++ js/src/name-model/index.js
@@ -1,2 +1,3 @@
 export { serializeNamesLastNameFirst } from './serialize-names-last-name-first';
 export { parseStandardName } from './parse-standard-name';
+export { validateStandardName } from './validate-standard-name';

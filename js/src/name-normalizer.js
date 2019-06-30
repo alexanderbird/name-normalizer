df428b2bca354e19c9cae10b306cc2333f53446c
Alexander Bird <alex@alexanderbird.software>
2019-06-29 23:28:47 -0600

    tcr commit

--- js/src/name-normalizer.js
+++ js/src/name-normalizer.js
@@ -1,7 +1,7 @@
 import { standardNameParser } from './standard-name-parser';
 import { AlphabeticalNameSerializer } from './alphabetical-name-serializer';
 
-export const normalize = name => {
+export function normalize(name) {
   const names = standardNameParser(name)
   return new AlphabeticalNameSerializer(names).serialize();
 }

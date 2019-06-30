68d4c8026bb3acd87594de53280bdeb49de30de5
Alexander Bird <alex@alexanderbird.software>
2019-06-30 09:10:19 -0600

    tcr commit

--- js/src/name-model/parse-standard-name.js
+++ js/src/name-model/parse-standard-name.js
@@ -3,10 +3,7 @@
 export function parseStandardName(nameString) {
   const cleanName = nameString.trim();
   const { rest, suffix } = getSuffix(cleanName);
-  const nameParts = rest.split(' ');
-  const first = nameParts.shift();
-  const last = nameParts.pop();
-  const middle = nameParts;
+  const { first, middle, last } = getNames(rest);
   return { first, middle, last, suffix }; /* type Names */
 }
 
@@ -14,5 +11,13 @@ function getSuffix(name) {
   const [rest, suffix, otherCrap] = name.split(', ');
   if(otherCrap) throw new Error('Too many commas');
   return { rest, suffix }
+}
+
+function getNames(nameString) {
+  const nameParts = nameString.split(' ');
+  const first = nameParts.shift();
+  const last = nameParts.pop();
+  const middle = nameParts;
+  return { first, middle, last };
 }
 

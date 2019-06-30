2c23206556ef06f397a8ac00749b3bee22f9e58b
Alexander Bird <alex@alexanderbird.software>
2019-06-30 08:52:38 -0600

    tcr commit

--- js/src/alphabetical-name-serializer.js
+++ js/src/alphabetical-name-serializer.js
@@ -2,30 +2,18 @@ import { WordJoiner } from './word-joiner';
 /* See interface Names in ./names.js */
 
 export function serializeNamesLastNameFirst(names /* type Names */) {
-    return new WordJoiner(names.last)
-      .comma(names.first)
-      .space(shortenMiddleNames(names.middle))
-      .comma(names.suffix)
-      .toString();
-  return new AlphabeticalNameSerializer(names).serialize();
+  return new WordJoiner(names.last)
+    .comma(names.first)
+    .space(shortenMiddleNames(names.middle))
+    .comma(names.suffix)
+    .toString();
 }
 
 
 function shortenMiddleName(middleName) {
-    return middleName.length === 1 ? middleName : middleName[0] + '.';
-  }
+  return middleName.length === 1 ? middleName : middleName[0] + '.';
+}
 
 function shortenMiddleNames(middleNames) {
   return middleNames.map(shortenMiddleName).join(' ');
-}
-
-
-export class AlphabeticalNameSerializer {
-  constructor(names /* type Names */) {
-    this._names = names;
-  }
-
-  serialize() {
-    const names = this._names;
-  }
 }

b42e640a3c01a6182fbf0227ba26c13151ceab7f
Alexander Bird <alex@alexanderbird.software>
2019-06-29 22:40:34 -0600

    tcr commit

--- js/src/name-normalizer.js
+++ js/src/name-normalizer.js
@@ -1,4 +1,9 @@
 export const normalize = name => {
-  const [first, last] = name.trim().split(' ');
+  const { first, last } = parseNames(name);
   return last ? [last, first].join(', ') : first;
+}
+
+function parseNames(nameString) {
+  const [first, last] = nameString.trim().split(' ');
+  return { first, last }
 }
